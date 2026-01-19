cae3265cebe1cfe79b2c27aef2bba7d7799cde92
HugoNicholls <p0089128@brookes.ac.uk>
2026-01-19 11:18:58 +0000

    Version format change

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'block_obu_learnanalytics';
-$plugin->release    = 'v1.12.8';      //Do global search/replace (without the v) when changing this
+$plugin->release    = '1.13.0.0';      //https://docs.google.com/document/d/1euRIJDufVxGANp1hyOA0arLS_4nlu1INFMZSadTvwdw/edit?tab=t.0
 $plugin->version    = 2025121800;   // yyyymmddvv
 $plugin->requires   = 2019052000; // Moodle v3.7.0
 // Supported value is any of the predefined constants MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE.
